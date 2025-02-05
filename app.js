fb6baab96e879a64501f6cbf8412ae7c2d66394c
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2025-02-05 12:24:31 +0100

    stage-1

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { StatusCodes } = require('http-status-codes');
+const cors = require('cors');
+
+const app = express();
+require('dotenv').config();
+
+app.use(express.json())
+app.use(cors());
+
+const {isPrime, isPerfect, isDigitSum, isArmStrong} = require('./utils/numberFunction');
+
+const PORT = process.env.PORT;
+
+app.get('/api/classify-number/', async (req, res) => {
+    const number = (req.query.num);
+    if(isNaN(number) || number < 1){
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            number: number,
+            error:"Invalid number"
+        })
+    }
+    try {
+        const response = await fetch(`http://numbersapi.com/${number}/math?json`);
+        const data = await response.json();
+
+        res.status(StatusCodes.OK).json({
+            number: number,
+            is_Perfect: isPerfect(number),
+            is_Prime: isPrime(number),
+            properties: isArmStrong(number),
+            digit_sum: isDigitSum(number),
+            fun_fact: data.text
+        })
+    } catch (error) {
+        res.status(StatusCodes.BAD_REQUEST).json({ 
+            error: "Error fetching number fact" 
+        });
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+
+
---
+++ utils/numberFunction.js
@@ -0,0 +1,55 @@
+const { StatusCodes } = require("http-status-codes");
+
+const isPrime = (number) =>{
+    if (number <= 2) return false;
+
+    for(let i = 2; i <= Math.sqrt(number); i++){
+        if (number % i === 0) return false;
+    }
+    return true;
+}
+
+const isArmStrong = (number) =>{
+    let numArray = number.toString().split('');
+    let numLength = numArray.length;
+
+    const is_Armstrong = numArray.map(num => parseInt(num)).reduce((acc, num) => acc + num**numLength, 0);
+    
+    if(number == is_Armstrong){
+        if(number % 2 === 0){
+            return ["isarmstrong", "even"];
+        }
+        else{
+            return ["isarmstrong", "odd"];
+        }
+    }
+    else{
+        if(number % 2 === 0){
+            return ["even"];
+        }
+        else{
+            return ["odd"];
+        }
+    } 
+}
+
+const isPerfect = (number) => {
+    if(number <= 1) return false;
+    let sum = 0;
+    
+    for(let i = 1; i <= number / 2; i++){
+        if(number % i === 0){
+            sum += i;
+        }
+    }
+    return sum == number;
+}
+
+const isDigitSum = (number) => {
+    let digitArray = number.toString().split('');
+
+    return digitArray.map(num => parseInt(num)).reduce((acc, num) => acc + num, 0)
+}
+
+
+module.exports = {isPrime, isPerfect, isArmStrong, isDigitSum};
