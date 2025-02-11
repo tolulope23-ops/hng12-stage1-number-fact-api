9284ffda8ede152a8f4749073c824fc335a6ebc2
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2025-02-11 08:14:04 +0100

    created an util folder for code modularity

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { StatusCodes } = require('http-status-codes');
 const cors = require('cors');
 
+const{isPrime, isPerfect, isDigitSum, isArmStrong, get_number_fact} = require('./utils/number_function')
+
 const app = express();
 require('dotenv').config();
 
@@ -9,100 +11,47 @@ app.use(express.json())
 app.use(cors());
 
 
-// Helper function to cehck if a number is prime
-const isPrime = (number) =>{
-    if (number < 2) return false;
-
-    for(let i = 2; i <= Math.sqrt(number); i++){
-        if (number % i === 0) return false;
-    }
-    return true;
-}
-
-// Helper function to cehck if a number is Armstrong
-const isArmStrong = (number) =>{
-    let num = Math.abs(number);
-    let numArray = num.toString().split('');
-    let numLength = numArray.length;
-
-    const is_Armstrong = numArray.map(num => parseInt(num)).reduce((acc, num) => acc + num**numLength, 0);
-    if(num == is_Armstrong){
-        if(num % 2 === 0){
-            return ["isarmstrong", "even"];
-        }
-        else{
-            return ["isarmstrong", "odd"];
-        }
-    }
-    else{
-        if(num % 2 === 0){
-            return ["even"];
-        }
-        else{
-            return ["odd"];
-        }
-    } 
-}
-
-// Helper function to cehck if a number is perfect
-const isPerfect = (number) => {
-    if(number <= 1) return false;
-    let sum = 0;
-    
-    for(let i = 1; i <= number / 2; i++){
-        if(number % i === 0){
-            sum += i;
-        }
-    }
-    return sum == number;
-}
-
-// Helper function to cehck if a number is digitSum
-const isDigitSum = (number) => {
-    let removeIndex;
-    let digitSum;
-    let digitArray = number.toString().split('');
-    if(digitArray[0] === '-'){
-        removeIndex = digitArray.slice(1);
-        digitSum = removeIndex.map(num => parseInt(num)).reduce((acc, num) => acc + num, 0)
-        const add = parseInt("-" + digitSum);
-        return add;
-    }
-    else{
-        digitSum = digitArray.map(num => parseInt(num)).reduce((acc, num) => acc + num, 0)
-        return digitSum;
-    }
-}
-
-// return number.toString().split('').reduce((acc, digit) => acc + Number(digit), 0);
-
 const PORT = process.env.PORT;
 
+
 app.get('/api/classify-number/', async (req, res) => {
-    const number = (req.query.num);  
-    if (!number || isNaN(number)) {
+    const number = req.query.number;  
+    if (!number) {
         return res.status(400).json({
-            error: true,
+            error: null,
             number: number
         });
     }
 
-    const num = parseInt(number, 10);
-    try {
+    const num = Number(number);
+    if(isNaN(num)){
+        return res.status(400).json({
+            error: num,
+            number: number
+        });
+    }
 
-        const response = await fetch(`https://numbersapi.com/${number}/math?json`);
-        const data = await response.json().text;
+    const properties = [];
+
+    if (isArmStrong(num)) 
+        properties.push("armstrong");
+    properties.push(num % 2 === 0 ? "even" : "odd");
+
+    try {
+           
+        const funFact = await get_number_fact(num);
 
         const result = {
-            num,
-            is_perfect: isPerfect(num),
+            number: num,
             is_prime: isPrime(num),
-            properties: isArmStrong(num),
+            is_perfect: isPerfect(num),
+            properties: properties,
             digit_sum: isDigitSum(num),
-            fun_fact: data
+            fun_fact: funFact
         }
         res.status(StatusCodes.OK).json(result)
-    } catch (error) {
+    }
+    catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({ 
             error: "Error fetching number fact" 
         });
---
+++ utils/number_function.js
@@ -0,0 +1,59 @@
+
+// Helper function to cehck if a number is prime
+const isPrime = (number) =>{
+    if (number < 2) return false;
+
+    for(let i = 2; i <= Math.sqrt(number); i++){
+        if (number % i === 0) return false;
+    }
+    return true;
+}
+
+// Helper function to cehck if a number is Armstrong
+const isArmStrong = (number) =>{
+    let num = Math.abs(number);
+    let numArray = num.toString().split('');
+    const numLength = numArray.length;
+
+    const is_Armstrong = numArray.map(num => parseInt(num)).reduce((acc, num) => acc + num**numLength, 0);
+    
+    return num == is_Armstrong;
+}
+
+// Helper function to cehck if a number is perfect
+const isPerfect = (number) => {
+    if(number <= 1) return false;
+    let sum = 0;
+    
+    for(let i = 1; i <= number / 2; i++){
+        if(number % i === 0){
+            sum += i;
+        }
+    }
+    return sum == number;
+}
+
+// Helper function to check if a number is digitSum
+const isDigitSum = (number) => {
+    let digit = Math.abs(number);
+    let digitArray = digit.toString().split('');
+    const digitSum = digitArray.map(num => parseInt(num)).reduce((acc, num) => acc + num, 0)
+
+    return number < 0 ? -digitSum : digitSum;
+    
+}
+
+const get_number_fact = async (number) =>{
+    try {
+        const response = await fetch(`http://numbersapi.com/${number}/math?json`);
+        const number_fact = await response.json();
+        
+        return number_fact.text;
+
+    } catch (error) {
+        res.status(400).json({ error: `Interresting fact about ${number} cannot be fetched`}) 
+    }
+}
+
+
+module.exports ={isPrime, isPerfect, isDigitSum, isArmStrong, get_number_fact}
